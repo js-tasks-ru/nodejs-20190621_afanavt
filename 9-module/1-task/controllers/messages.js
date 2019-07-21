c8a9c51c86db339d895d8d3e4c8f89130a598ac2
Afanasev <afanavt@gmail.com>
2019-07-22 02:26:40 +0700

    solution for 9-module

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,7 +1,14 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {
-    messages: [],
-  };
+    const msgs = await Message.find({text: 'message'}).limit(20);
+    const messages = msgs.map(msg => ({
+            id: msg.id,
+            text: msg.text,
+            user: msg.user,
+            date: msg.date
+        })
+    );
+
+    ctx.body = { messages };
 };
--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -1,20 +1,46 @@
 const socketIO = require('socket.io');
+const mongoose = require('mongoose');
 
 const Session = require('./models/Session');
 const Message = require('./models/Message');
 
 function socket(server) {
-  const io = socketIO(server);
-  
-  io.use(async function(socket, next) {
-    next();
-  });
-  
-  io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
-  });
-  
-  return io;
+    const io = socketIO(server);
+
+    io.use(async function(socket, next) {
+        const { token  } = socket.handshake.query;
+        if(!token){
+            return next(new Error("anonymous sessions are not allowed"));
+        }
+        const session = await Session.findOne({ token  }).populate('user');
+        if(!session){
+            return next(new Error('wrong or expired session token'));
+        }
+
+        socket.user = session.user;
+        next();
+    });
+
+    io.on('connection', function(socket) {
+        socket.on('message', async (msg) => {
+            const date = new Date();
+            socket.emit('user_message', {
+                user: socket.user.displayName,
+                text: msg,
+                date,
+
+            });
+
+            await Message.create({
+                user: socket.user.displayName,
+                text: msg,
+                date,
+                chat: socket.user
+            });
+        });
+    });
+
+    return io;
 }
 
 module.exports = socket;
